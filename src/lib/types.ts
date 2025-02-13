b49e8c5bd57c1cf7db2dbe14a5e18a89a6db113f
Selaelo2 <selaelolanga10@gmail.com>
2025-02-13 22:37:40 +0200

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Add these types to your existing types.ts file
 
+import { ReactNode } from "react";
+
 export interface JobFilters {
+  id: any;
+  budget: any;
+  createdAt: string | number | Date;
+  deadline: any;
+  description: ReactNode;
+  title: ReactNode;
   search: string;
   minBudget: number;
   maxBudget: number;
